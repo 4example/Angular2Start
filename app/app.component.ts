29a00baf7d3f26d566b99d6e545a5be024f115a7
Danti <r.kawalec@vp.pl>
2016-05-08 12:46:30 +0200

    start

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by danti on 04.05.2016.
+ */
+
+
+import { Component } from '@angular/core';
+
+@Component({
+
+    selector: 'my-app',
+    template: '<br><br><br><br><h1 align="center">Loading... </h1>' +
+              '<br><br><h1 align="center">4Example mode on... :)</h1>' +
+              '<br><br><h6 align="center">Powered by Angular2</h6>'
+})
+
+export class AppComponent { }
+
---
+++ app/main.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by danti on 04.05.2016.
+ */
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
+
